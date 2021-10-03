0e5752aafec43a71c22f01c3dee90c612035b807
Grzegorz Blajer <43644352+gblajer@users.noreply.github.com>
2021-10-03 20:57:02 +0200

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,84 @@
+resource "azurerm_virtual_network" "vd-network" {
+  name                = "vd-network"
+  location            = data.azurerm_resource_group.main_rg.location
+  resource_group_name = data.azurerm_resource_group.main_rg.name
+  address_space       = ["192.168.88.0/21"]
+  #dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
+resource "azurerm_virtual_network" "bastion-network" {
+  name = "bastion-network"
+  location = data.azurerm_resource_group.main_rg.location
+  resource_group_name = data.azurerm_resource_group.main_rg.name
+  address_space = ["10.46.145.0/26"]
+}
+resource "azurerm_subnet" "AzureBastionSubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.bastion-network.name
+  address_prefixes     = azurerm_virtual_network.bastion-network.address_space
+}
+
+resource "azurerm_virtual_network" "dc-network" {
+  name = "dc-network"
+  location = data.azurerm_resource_group.main_rg.location
+  resource_group_name = data.azurerm_resource_group.main_rg.name
+  address_space = ["192.168.87.0/24"]
+}
+resource "azurerm_subnet" "dc-subnet" {
+  name                 = "dc-subnet"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.dc-network.name
+  address_prefixes     = azurerm_virtual_network.dc-network.address_space
+}
+resource "azurerm_subnet" "cs-subnet" {
+  name                 = "cs-subnet"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.vd-network.name
+  address_prefixes     = ["192.168.88.0/24"]
+}
+
+resource "azurerm_subnet" "linuxvdi-subnet" {
+  name                 = "linuxvdi-subnet"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.vd-network.name
+  address_prefixes     = ["192.168.89.0/24"]
+}
+
+resource "azurerm_subnet" "windowsvdi-subnet" {
+  name                 = "windowsvdi-subnet"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.vd-network.name
+  address_prefixes     = ["192.168.90.0/24"]
+}
+
+resource "azurerm_subnet" "mgmt-subnet" {
+  name                 = "mgmt-subnet"
+  resource_group_name  = data.azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.vd-network.name
+  address_prefixes     = ["192.168.91.0/24"]
+}
+resource "azurerm_virtual_network_peering" "peering_dc-network_bastion-network" {
+  name                      = "peering_dc-network_bastion-network"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.dc-network.name
+  remote_virtual_network_id = azurerm_virtual_network.bastion-network.id
+}
+resource "azurerm_virtual_network_peering" "peering_bastion-network_dc-network" {
+  name                      = "peering_bastion-network_dc-network"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.bastion-network.name
+  remote_virtual_network_id = azurerm_virtual_network.dc-network.id
+}
+resource "azurerm_virtual_network_peering" "peering_dc-network_vd-network" {
+  name                      = "peering_dc-network_vd-network"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.dc-network.name
+  remote_virtual_network_id = azurerm_virtual_network.vd-network.id
+}
+
+resource "azurerm_virtual_network_peering" "peering_vd-network_dc-network" {
+  name                      = "peering_vd-network_dc-network"
+  resource_group_name       = data.azurerm_resource_group.main_rg.name
+  virtual_network_name      = azurerm_virtual_network.vd-network.name
+  remote_virtual_network_id = azurerm_virtual_network.dc-network.id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+#ID subskrypcji
+variable "sub_id" {
+  type        = string
+  default     = "8c42b3e1-e80e-4556-956c-fb4c9ced0810"
+}
+variable "resource_group_name" {
+  type = string
+  default = "WVD-TEST"
+}
